9f117a238a30597a86df256fe18f36a2cc26f62c
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2025-03-21 19:05:28 +0530

    Modified Next number from the integer passed

--- C Language/Next_number.c
+++ C Language/Next_number.c
@@ -1,12 +1,14 @@
 // Return the Next Number from the Integer Passed
-#include<stdio.h>
+#include <stdio.h>
 
-int next_number_of(int passed_integer){
+int next_number_of(int passed_integer)
+{
     int next_number = passed_integer + 1;
     return next_number;
 }
 
-int main(){
+int main()
+{
     int num = 1;
     printf("Next number to %d is %d", num, next_number_of(num));
 }
