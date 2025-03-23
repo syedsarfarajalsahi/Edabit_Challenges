f62e30398cfca3cd62cc59cc2946a4f5d3f43808
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2025-03-23 21:59:21 +0530

    Number of Sticker on n sided Rubik's Cube

---
+++ C Language/Number_of_sticker.c
@@ -0,0 +1,13 @@
+// Number of Sticker on Rubik's Cube with side length
+#include <stdio.h>
+
+int how_many_stickers(int side_length)
+{
+    return (side_length * side_length) * 6;
+}
+
+int main()
+{
+    int side_length = 2;
+    printf("Number of stickers needed for 3X3 Rubik's Cube is %d", how_many_stickers(side_length));
+}
