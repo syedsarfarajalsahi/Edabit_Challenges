d81f0f54faefd79cdd4af583ba57228afe4e67b5
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2025-04-02 11:59:55 +0530

    Tried another side length

--- C Language/Number_of_sticker.c
+++ C Language/Number_of_sticker.c
@@ -8,6 +8,6 @@ int how_many_stickers(int side_length)
 
 int main()
 {
-    int side_length = 2;
+    int side_length = 456;
     printf("Number of stickers needed for 3X3 Rubik's Cube is %d", how_many_stickers(side_length));
 }
