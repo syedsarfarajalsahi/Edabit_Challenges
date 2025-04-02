f899eeacdc33f20a80d4dc068031119e70f95367
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2025-04-02 11:51:58 +0530

    modified in line 9 and 11 into given parameter

--- C Language/Not_not_not_true.c
+++ C Language/Not_not_not_true.c
@@ -6,14 +6,14 @@ void not_not_not_true(int number_of_times, char true_or_false[])
 {
     int is_even = (number_of_times % 2) ? 0 : 1;
     if (strcmp(true_or_false, "true") == 0)
-        printf("%s", is_even ? "true" : "false");
+        printf("%s", is_even ? true_or_false : "false");
     else if (strcmp(true_or_false, "false") == 0)
-        printf("%s", is_even ? "false" : "true");
+        printf("%s", is_even ? true_or_false : "true");
 }
 
 int main()
 {
     char true_false[] = "false";
-    int number_of_times = 2;
+    int number_of_times = 9;
     not_not_not_true(number_of_times, true_false);
 }
