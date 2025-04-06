70ad745d9b6f36b17bc506850cd1cf4da7ff05ec
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2025-04-06 09:39:16 +0530

    Char to ASCII

---
+++ C Language/Char_to_ASCII.c
@@ -0,0 +1,13 @@
+// Char to ASCII
+#include <stdio.h>
+
+int ctoa(char alphabet_letter)
+{
+    return alphabet_letter;
+}
+
+int main()
+{
+    char alphabet_letter = 'f';
+    printf("%d", ctoa(alphabet_letter));
+}
