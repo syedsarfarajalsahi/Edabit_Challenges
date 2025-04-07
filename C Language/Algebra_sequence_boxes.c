a2851e20c378e3746874f10f70c018c57125076b
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2025-04-07 11:20:02 +0530

    Putting rules in the comment section

--- C Language/Algebra_sequence_boxes.c
+++ C Language/Algebra_sequence_boxes.c
@@ -9,6 +9,14 @@ int box_seq(int steps)
         return steps + 2;
 }
 
+/*
+Here is the rule for this calculation:
+1. Step 0: start with 0;
+2. Step 1: Add 3;
+3. Step 2: Subtract 1
+4. Repeat Step 1 & and 2 ...
+*/
+
 int main()
 {
     int steps = 5;
