6fd341edabf72019fa28b4bbe6a89d5692db6677
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2025-04-10 10:59:55 +0530

    Change in Rules in Comment

--- C Language/Tile_team_tactics.c
+++ C Language/Tile_team_tactics.c
@@ -3,13 +3,13 @@
 
 /*
 Rule:
-1. if a == b --> true
+1. if a == b --> false
 2. if a < b & b - a <= 6 --> true
 */
 
 void possible_bonus(int a, int b)
 {
-    if (b - a >= 0 && b - a <= 6)
+    if (a < b && b - a <= 6)
         printf("true\n");
     else
         printf("false\n");
@@ -17,6 +17,6 @@ void possible_bonus(int a, int b)
 
 int main()
 {
-    int a = 5, b = 3;
+    int a = 3, b = 7;
     possible_bonus(a, b);
 }
