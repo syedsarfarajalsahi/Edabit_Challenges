aa807f47edb6ef1cd8b08aa1ab9e0c965f0e50ed
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2025-09-06 18:23:26 +0530

    Next Perfect Square

---
+++ C Language/Next_Perfect_Square.c
@@ -0,0 +1,22 @@
+// Next Perfect Square
+#include <stdio.h>
+#include <math.h>
+
+int next_Square(int squared_number)
+{
+    double sqrt_number = sqrt(squared_number);
+    if (floor(sqrt_number) == sqrt_number)
+    {
+        return pow(sqrt_number + 1, 2);
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int main()
+{
+    int n = 114;
+    printf("%d", next_Square(n));
+}
