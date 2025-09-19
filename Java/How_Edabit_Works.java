ec0e8a49e7fe52577aed4aa68dd0c20411eb6a71
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2025-09-19 08:28:49 +0530

    How_Edabit_Works

---
+++ Java/How_Edabit_Works.java
@@ -0,0 +1,9 @@
+public class How_Edabit_Works {
+    public static boolean returnTrue(){
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(returnTrue());
+    }
+}
