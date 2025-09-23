2dd38ebc5b505bdae7e0077f4ca4039dc564a84f
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2025-09-23 13:05:02 +0530

    Returnt the next number from the Integer passed

---
+++ Java/Return_the_next_number.java
@@ -0,0 +1,11 @@
+public class Return_the_next_number {
+    public static void addition(int given_number) {
+        int sum = given_number + 1;
+        System.out.println(sum);
+    }
+    
+    public static void main(String[] args) {
+        int any_number = 4;
+        addition(any_number);
+    }
+}
