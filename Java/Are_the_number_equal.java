2ec4ed1a3806eebf974d46fa3b16ae20db0d3e4f
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2025-09-23 13:15:21 +0530

    Are the number equal

---
+++ Java/Are_the_number_equal.java
@@ -0,0 +1,13 @@
+public class Are_the_number_equal {
+    public static void is_same_number(int a, int b) {
+        if (a == b)
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+
+    public static void main(String[] args) {
+        int x = 4, y = 5;
+        is_same_number(x, y);
+    }
+}
