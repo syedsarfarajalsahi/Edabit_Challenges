9925c6d549e7005ca0bba8668dd75f0e01ea1996
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2025-09-23 10:55:33 +0530

    Return the sum of two number

---
+++ Java/Return_the_sum_of_two_number.java
@@ -0,0 +1,10 @@
+public class Return_the_sum_of_two_number {
+    public static void sum_of_two_numbers(int a, int b){
+        int result = a + b;
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        int a = 3, b = 2;
+        sum_of_two_numbers(a, b);
+    }
+}
