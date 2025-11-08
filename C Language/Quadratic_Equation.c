f3a01285111952b67cddd71085e73b26fae442a5
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2025-11-08 11:27:17 +0530

    Quadratic equation root

--- C Language/Quadratic_Equation.c
+++ C Language/Quadratic_Equation.c
@@ -4,12 +4,19 @@
 
 int quadratic_equation(int a, int b, int c)
 {
-    int D = sqrt(pow(b, 2) - ((a * c) * 4));
-    int root = (D - b)/(a*2); // only the positive part
+    // D - discriminant of the quadratic equation
+    double root, D;
+    D = pow(b, 2) - (4 * (a * c));
+    if (D < 0)
+    {
+        printf("Root is imaginary number.");
+        return 0;
+    }
+    root = (sqrt(D)-b)/(2*a);
     return root;
 }
 
 int main()
 {
-    printf("%d", quadratic_equation(1, -12, -28));
+    printf("%d", quadratic_equation(1, -4, 1));
 }
