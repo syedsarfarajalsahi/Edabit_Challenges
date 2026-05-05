504e0ab6b1d793032b07d8faaba8c5e8de3ee892
syedsarfarajalshi <syedsarfarajalsahi@gmail.com>
2026-05-05 10:05:53 +0530

    Removing comments

--- JS/Convert_age_into_days.js
+++ JS/Convert_age_into_days.js
@@ -1,6 +1,4 @@
 function calc_age(age_in_years) {
-    // Assume all those years have only 365 days. We ignore leap years
-    // inorder not to make it complicated in coding
     const age_in_days = age_in_years * 365;
     console.log(age_in_years + ' years old is equals to ' + age_in_days + ' days old');
 }
